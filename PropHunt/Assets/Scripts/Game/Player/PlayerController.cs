e83dc1bc1178c2670bf89c3fec17b839f0361ab6
Sabien <ambid17@gmail.com>
2021-12-27 12:01:40 -0600

    disable player scripts

--- PropHunt/Assets/Scripts/Game/Player/PlayerController.cs
+++ PropHunt/Assets/Scripts/Game/Player/PlayerController.cs
@@ -1,8 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
-
+    [SerializeField] private GameObject _camera;
+    [SerializeField] private PlayerMovement _playerMovement;
+    [SerializeField] private CameraMove _cameraMove;
+    [SerializeField] private CharacterController _characterController;
+    private void Start()
+    {
+        if (!IsLocalPlayer)
+        {
+            _camera.SetActive(false);
+            _playerMovement.enabled = false;
+            _cameraMove.enabled = false;
+            _characterController.enabled = false;
+        }
+    }
 }
