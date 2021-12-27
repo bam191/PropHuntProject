b7d3a2c90680c80cb0411c30a2268cdb1c6e412b
Sabien <ambid17@gmail.com>
2021-12-27 10:30:40 -0600

    added player controller

---
+++ PropHunt/Assets/Scripts/Player/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class InputManager
+{
+    public static bool GetKeyDown(string keyName)
+    {
+        return Input.GetKeyDown(HotKeyManager.Instance.GetKeyFor(keyName));
+    }
+
+    public static bool GetKeyUp(string keyName)
+    {
+        return Input.GetKeyUp(HotKeyManager.Instance.GetKeyFor(keyName));
+    }
+
+    public static bool GetKey(string keyName)
+    {
+        return Input.GetKey(HotKeyManager.Instance.GetKeyFor(keyName));
+    }
+
+    public static bool GetModifiedKeyDown(string keyName)
+    {
+        return Input.GetKey(HotKeyManager.Instance.GetKeyFor(PlayerConstants.ModifierKey)) 
+            && Input.GetKeyDown(HotKeyManager.Instance.GetKeyFor(keyName));
+    }
+}
