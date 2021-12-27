78fa59696d1ba20978ac890fbb3cf4cd20afc13c
Sabien <ambid17@gmail.com>
2021-12-26 21:54:27 -0600

    fix singleton

--- PropHunt/Assets/Scripts/InputController.cs
+++ PropHunt/Assets/Scripts/InputController.cs
@@ -2,21 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputController : MonoBehaviour
+public class InputController : Singleton<InputController>
 {
-    private static InputController _instance;
-    public static InputController Instance
-    {
-        get
-        {
-            return _instance;
-        }
-    }
-
     private void Awake()
     {
-        _instance = this;
-
         DontDestroyOnLoad(gameObject);
     }
 
--- PropHunt/Assets/Scripts/MainMenu/MainMenuController.cs
+++ PropHunt/Assets/Scripts/MainMenu/MainMenuController.cs
@@ -21,8 +21,6 @@ public class MainMenuController : Singleton<MainMenuController>
 
     private void Awake()
     {
-        SetInstance(this);
-
         SetState(eMainMenuState.MainMenu);
     }
 
